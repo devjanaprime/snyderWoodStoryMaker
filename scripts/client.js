408ebbadd45b890920aeec1d50a630f93c923959
Dev Jana <dev@primeacademy.io>
2018-08-16 09:59:24 -0500

    cannot believe I did not commit before...

---
+++ scripts/client.js
@@ -0,0 +1,242 @@
+////////////////////////////////////////////|||\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+//                              Snyder Wood Story Maker                                   \\
+//                                   by  @devjana                                         \\
+////////////////////////////////////////////|||\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/*******************************************************************************************
+dddddddddddddddddddddddddddddddddddddddddddddddddddddddddmmmmmmmmmddmmmmmmmmmmmmmmmmmmmmmmdd
+ddddddddddddddddddddddddddddddddddddddddddddddddddddmdhyyhdddddmmmmmmmmmmmmdddddhhhhhhyyyyyy
+dddddddddddddddddddddddddddddddddddddddddddddddddddddhssoosssyhddddmddddhhhhyyhhyyysssssssss
+dddddddddddddddddddddddddddddddddddddddddddddddddddddhyo++++ooosyyhhhyyyyyhhhyyyssssssssoooo
+ddddddddddddddddddddddddddddddddddddddddddddddddddddddho+++oossyyyyssyyyhhyyyyyssssooooooooo
+ddddddddddddddddddddddddddddhdddddddddddddddddddddddddhyoooosssssssyyhyyyyssssssoooooooooooo
+dddddddddddddddddddddddhyys++oyhhhyysssossssyyhhddddhysooooosssoosyhyyyyssssoooooooooooooooo
+ddddddddddddddddddddddys+/:::/+oo+///::::////+++osssooooooossoosyhyyyyssssooooooo++o++++++oo
+ddddddddddddddddddmmdsoo+:-:////:::::----::::::////+++++ooooooyhhhyyyssooooo+++++++++++++++o
+hddddddddddddddddmNmyo+oo/////:::---------::::::::::::////+++osyyysssooooo++++++++++++++++++
+hhhhddddddddddddmNNho+o+////:::----------------------:::::::///++oooooo+++++++++++++++++++++
+hhddhdddddddddddmNmsoo+///:::---------...----------------::::::::/++oo++++++++++++++++++++++
+hhhhhhdddddhhddmNdso+/++/::----------..........---:-------------::://+++++++++++++++++++++++
+hhhhhhhhhhhdddmNNyo+/++//:::-------..............-......---------::::/++oo++++++++++++++++++
+hhhhhdhhhhhhdmNNmyoooo+++//:--......`````...``.............-------::::/+++++oo++++++++++++++
+dddddddhhhdddmNNms/+ssoshddhyo:.```````````````...```.......-------:::/++////++++++oo+++++++
+hhhhhhhhhhhhdmNNNhsys+ohdmNmhhy/.````````````````````........-------:://+/:-------:/++++++++
+hhhhhhhhhhhhddmNNNmy+/ohmNmmdhho-..``````````````````....----------::::/+/:::-------::/+++++
+hhhhhdddhhhhhddmNNms/:+ohmmmmdho:-..``````````````.--///::/:::------:::///:/:-----:::::+o+++
+hhhhhhhhhddhhddNNNd+:::::+ossss/:--..```````````.-/+oyysosyyys+:--::::///::::::://////:/+oo+
+ddddhhhhddhhhdmNNNh+:-::-....--..--.``````````.-//oyyhhhdddmmdhs//+o+////:://///++++////+oo+
+hhhhhdddddhhddmNNNh+:-::-...`.....```````````-/oyyyhhsshmmmmNNNmyshdho++/:::///:/++///:-:+oo
+hhhhhdhhhhhhddmNNNh+///:-...........````.```./syhhhho:omhsyhmNNNmdmNmy++::::-:////++///:-/++
+hhhhdddhhhhhhdmNNNd++/:-.....-----........``-+syyhdho/hmmddmNNNmmmNNNho+::::-:+++++///::-//+
+hhhhhhhhhhhhhddNNNNho/:-...-::--------.......-+sssyyhhdmmNNNNNmdmmmNmyo//::::://+oo+/::-:///
+hhhhhhhhhhhhhhdmNNNdo:-.--::::--------........-/ossyyhddmmmmmddhdmmNds//////+//++ss+:--:/++/
+hhhhhhhhhhhhhhhdmNmdo:-::::::/::::---...----...:oyyhhhdddmmddhhhdmNmy/-::://ssoso+/:---:++++
+hhhhhhhhhhhhhhddmNmdy+//::--:::-----...-:::-...-+yhhhdddmmdhhhhddmmh+--::::/+oyyo/----/oo+++
+hhhhhhhhhhhhhhhdmmyys+/:--:::-..----...-----....-+yddmmmddhhhddmmmy+::::://///+sys+/++oo++++
+hhhhhhhhhhhhhhhdmdso+/::+oo/:::-:::--....---.....-+yhmmmdddddmmmmy+:::://::::/++oossoo++++++
+hhhhhhhhhhhhhhdmmho///+ydddhysoo+/:-.....----.....-:+shdhhdmmmmhs+/////:::-:::/+++/+++++++++
+hhhhhhhhhhhhhhdmmho///+ydddhysoo+/:-.....----.....-:+shdhhdmmmmhs+/////:::-:::/+++/+++++++++
+hhhhhhhhhhhhhdmmms/:/sdNNNNNmmmdhyo/-...--::--...---/+o++osyyso+////:::-----::/+++/+++++++++
+hhhhhhhhhhhhhdmmms/:/sdNNNNNmmmdhyo/-...--::--...---/+o++osyyso+////:::-----::/+++/+++++++++
+hhhhhhhhhhhhhdmmms/:/sdNNNNNmmmdhyo/-...--::--...---/+o++osyyso+////:::-----::/+++/+++++++++
+hhhhhhhhhhhhhdmmh+::odmNmmmmmmmmmmdy/-..--::::---::///::::///+////::---..---::/+++//++oo++++
+hhhhhhhhhhhhhdmmh+::odmNmmmmmmmmmmdy/-..--::::---::///::::///+////::---..---::/+++//++oo++++
+hhhhhhhhhhhhdmNmh+::odmNNmdddddmmmNdo-----:/::::///::::::::///::::--.....---::/+////+oooo+++
+hhhhhhhhhhhhdmNmh+::odmNNmdddddmmmNdo-----:/::::///::::::::///::::--.....---::/+////+oooo+++
+hhhhhhhhhhhdmmmdy+::+hmNNmmmmmmmmmmh+-----://////::-----:///::----.......---:://////+ooo++++
+hhhhhhhhhhhdmmmdy+::+hmNNmmmmmmmmmmh+-----://////::-----:///::----.......---:://////+ooo++++
+hhhhhhhhhhhdmddhyo+::+ydmmmmmmmmmdh+:--.--://///::----:://::-----........---://////++oooo+++
+hhhhhhhhhhhdmddhyo+::+ydmmmmmmmmmdh+:--.--://///::----:://::-----........---://////++oooo+++
+hhhhhhhhhhhdhssyyys+///+ymmddmdhs+/------:////::::--:::::------.........---:///////++ooo++++
+hhhhhhhhhhhdhssyyys+///+ymmddmdhs+/------:////::::--:::::------.........---:///////++ooo++++
+hhhhhhhhhhdho/+osyyoosssyhsssso/:-------:////:::--::::------...........---::::////++oooo++++
+hhhhhhhhhhdho/+osyyoosssyhsssso/:-------:////:::--::::------...........---::::////++oooo++++
+hhhhhhhhhdho/////osysooshdhyso+/:----::////::-----::------...........---:::::////++ooooo++++
+hhhhhhhhhdho/////osysooshdhyso+/:----::////::-----::------...........---:::::////++ooooo++++
+hhhhhhhhdho+///:::/ossooooossso+///////:::--------::----...........----::::::///++ooooooo+++
+hhhhhhhhds///:::----:/+oooooo++++///:::------.---:::--............-----:::::///++ooooooo+ooo
+hhhhhhhhds///:::----:/+oooooo++++///:::------.---:::--............-----:::::///++ooooooo+ooo
+hhhhhhhdho:::::---....--:://///::::::::----------::-.............------::::///++oossoooooooo
+hhhhhhhdho:::::---....--:://///::::::::----------::-.............------::::///++oossoooooooo
+hhhhhhhdho:::::---....--:://///::::::::----------::-.............------::::///++oossoooooooo
+hhhhhhhhs+::::-----.....-------------::------------.............-------::////++osssooo+ooooo
+hhhhhhhhs+::::-----.....-------------::------------.............-------::////++osssooo+ooooo
+hhhhhhs+//::::-----......---------------------------...........------::://///++ossooo+ooosss
+hhhhhhs+//::::-----......---------------------------...........------::://///++ossooo+ooosss
+hhhhyo/:----:--..----.......------------------------......---.------:::::////+ossoo++oooosss
+hhhhyo/:----:--..----.......------------------------......---.------:::::////+ossoo++oooosss
+hhyo/--....-----..---------------------------------.........-------:::::::///osso++++ooossyy
+hhyo/--....-----..---------------------------------.........-------:::::::///osso++++ooossyy
+hy+:-....-...-----------------------------------..............------:::::://+ooo++++oosssyhd
+y+:--..-----..--------:::::--------..----..-..................------::::://+oo+++++oossyhddd
+
+My sister's beloved dog Lulu was in her last days as I started this. Dedicating this
+small corner of code to Lulu and the joy she brought my sister, her husband, their kids,
+& the stories we'll be telling about Lulu for the years to come. Dogs are awesome.
+If you've got one, give it some love.
+******************************************************************************************/
+
+let verbose = true;
+const beats = [];
+const images = [];
+
+let story = {
+    title: 'noTitle',
+    author: 'noAuthor',
+    scenes: []
+}
+
+class Scene{
+  constructor( copyText, imageIndex, title ) {
+    if( verbose ) console.log( 'instantiating Scene:',copyText, imageIndex, title );
+    this.copyText = copyText; 
+    this.imageIndex = imageIndex;
+    this.title = title;
+  } //ned constructor
+} // end Scene
+
+let currentBeat = 0;
+
+let addBeat = ( newBeat ) =>{
+  if( verbose ) console.log( 'in newBeat: ', newBeat );
+  beats.push( newBeat );
+} // end addBeat
+
+let addImage = ( newImage ) =>{
+  if( verbose ) console.log( 'in addImage: ', newImage );
+  images.push( newImage );
+} // end addBeat
+
+let clearInputs = () =>{
+  document.getElementById( 'sceneCopyTextIn' ).value = '';
+  document.getElementById( 'sceneCTitleIn' ).value = '';
+}
+
+let displayCurrentScene = () => {
+  if( verbose ) console.log('displaying scene:', currentBeat, story.scenes[ currentBeat ] );
+  document.getElementById( 'sceneTitle' ).innerHTML = story.scenes[ currentBeat ].title;
+  document.getElementById( 'sceneImage' ).innerHTML = '<img src="' + images[ story.scenes[ currentBeat ].imageIndex ] + '">';
+  document.getElementById( 'sceneCopyText' ).innerHTML = story.scenes[ currentBeat ].copyText;
+} // end displayCurrentPanel
+
+let init = () =>{
+  setupBeats();
+  setupImages();
+  initStories();
+} // end init
+
+let initStories = () =>{
+  for( beat in beats ){
+    story.scenes.push( new Scene( 'copy text ' + beat, beat, 'title ' + beat ) );
+  }
+} // let initStories
+
+let nextImage = () =>{
+  if( verbose ) console.log( 'in nextPanel' );
+  story.scenes[ currentBeat ].imageIndex++;
+  displayCurrentScene();
+} // end nextImage
+
+let nextScene = () =>{
+  if( verbose ) console.log( 'in nextScene' );
+  currentBeat++;
+  if( currentBeat > story.scenes.length-1 ){
+    currentBeat = 0;
+  } // end wrap
+  saveScene();
+  displayCurrentScene();
+} // end nextPanel
+
+let prevScene = () =>{
+  if( verbose ) console.log( 'in prevScene' );
+  currentBeat--;
+  if( currentBeat < 0 ){
+    currentBeat = story.scenes.length-1;
+  } // end wrap
+  saveScene();
+  displayCurrentScene();
+} // end prevScene
+
+let saveScene = () =>{
+  if( verbose ) console.log( 'in saveScene' );
+  if( document.getElementById( 'sceneCopyTextIn' ).value != '' ){
+    if( verbose ) console.log( 'saving scene Title' );
+    story.scenes[ currentBeat ].copyText = document.getElementById( 'sceneCopyTextIn' ).value;
+  }
+  if( document.getElementById( 'sceneCopyTextIn' ).value != '' ){
+    if( verbose ) console.log( 'saving scene Title' );
+    story.scenes[ currentBeat ].title = document.getElementById( 'sceneTitleIn' ).value;
+  }
+  updateSceneDetails();
+} // end saveScene
+
+let saveStory = () =>{
+  story.title = document.getElementById( 'storyTitleIn' ).value;
+  story.author = document.getElementById( 'storyAuthorIn' ).value;
+  updateStoryDetails();
+} // end saveStory
+
+let setupBeats = () =>{
+  if( verbose ) console.log( 'in setupBeats' );
+  /// - temp
+    // setup default beats
+      addBeat( 'Opening Image' );
+      addBeat( 'Theme Stated' );
+      addBeat( 'Set-Up' );
+      addBeat( 'Catalyst' );
+      addBeat( 'Debate' );
+      addBeat( 'Break into Two' );
+      addBeat( 'B Story' );
+      addBeat( 'Fun and Games' );
+      addBeat( 'Midpoint' );
+      addBeat( 'Bad Guys Close In' );
+      addBeat( 'All Is Lost' );
+      addBeat( 'Dark Night of the Soul' );
+      addBeat( 'Break into Three' );
+      addBeat( 'Finale' );
+      addBeat( 'Final Image' );
+    // setup default images
+  /// - end temp
+} // ens setupBeats
+
+let setupImages = () =>{
+  if( verbose ) console.log( 'in setupImages' );
+  /// - temp
+    // setup default Images
+      addImage( 'panels/0.png' );
+      addImage( 'panels/1.png' );
+      addImage( 'panels/2.png' );
+      addImage( 'panels/3.png' );
+      addImage( 'panels/4.png' );
+      addImage( 'panels/5.png' );
+      addImage( 'panels/6.png' );
+      addImage( 'panels/7.png' );
+      addImage( 'panels/8.png' );
+      addImage( 'panels/9.png' );
+      addImage( 'panels/10.png' );
+      addImage( 'panels/11.png' );
+      addImage( 'panels/12.png' );
+      addImage( 'panels/13.png' );
+      addImage( 'panels/14.png' );
+      addImage( 'panels/15.png' );
+      addImage( 'panels/16.png' );
+      addImage( 'panels/17.png' );
+      addImage( 'panels/18.png' );
+      addImage( 'panels/19.png' );
+      addImage( 'panels/20.png' );
+      addImage( 'panels/21.png' );
+    // setup default images
+  /// - end temp
+} // end setupImages
+
+let updateSceneDetails = () => {
+  document.getElementById( 'sceneTitle' ).innerHTML = story.scenes[ currentBeat ].title;
+  document.getElementById( 'sceneCopyText' ).innerHTML = story.scenes[ currentBeat ].copyText;
+} // end updateStoryDetails
+
+let updateStoryDetails = () => {
+  document.getElementById( 'storyTitle' ).innerHTML = story.title;
+  document.getElementById( 'storyAuthor' ).innerHTML = story.author;
+} // end updateStoryDetails
+
+init();
+nextScene();
